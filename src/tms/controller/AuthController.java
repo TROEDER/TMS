71fe7edb5ccbd1c9872a9ab1a2be1fd3fa306478
AppArchivesAdmin <admin@app-archives.com>
2017-09-16 22:10:23 +0200

    Login-Frame added

--- src/tms/controller/AuthController.java
+++ src/tms/controller/AuthController.java
@@ -5,9 +5,14 @@
  */
 package tms.controller;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,7 +20,22 @@ import javafx.fxml.Initializable;
  * @author troeder
  */
 public class AuthController implements Initializable {
-
+    
+    @FXML
+    private Button btnLogin;
+    
+    @FXML
+    private void btnLoginPressed(ActionEvent event) throws Exception {
+        URL authViewURL = new File("src\\tms\\view\\App.fxml").toURI().toURL();
+        //Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
+        Parent root = FXMLLoader.load(authViewURL);
+       
+            Scene scene = new Scene(root);
+            Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            appStage.setScene(scene);
+            appStage.centerOnScreen();
+            appStage.show();
+    }
     /**
      * Initializes the controller class.
      */
